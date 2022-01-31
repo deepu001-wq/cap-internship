9a0c17630d65a8b0e1456bd3eee0cc7f81fe6f51
sudeepthi <telugusudeepthi001@gmail.com>
2022-01-31 16:58:39 +0530

    3rd day programs

---
+++ ObjectClass/ObjectClass/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ObjectClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            object sampleObj = new object();
+            object sampleObj2 = new object();
+            int i = 21;
+
+            bool result = Equals(sampleObj, sampleObj2);
+            Console.WriteLine(result);
+            Type t1 = sampleObj.GetType();
+            Type t2 = i.GetType();
+            Console.WriteLine(t1.Name);
+            Console.WriteLine(t1.Namespace);
+            Console.WriteLine(t1.BaseType);
+            Console.WriteLine(t2.Name);
+            Console.WriteLine(t2.Namespace);
+            Console.WriteLine(t2.BaseType);
+        }
+    }
+}
---
+++ equalsanddoubleequals/equalsanddoubleequals/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace equalsanddoubleequals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Sudeepthi";
+            string Myname = name;
+
+            var doubleeql = (name == Myname);//true
+            var eql = name.Equals(Myname);//true
+            Console.WriteLine(doubleeql);
+            Console.WriteLine(eql);
+            Console.WriteLine("====================================");
+            object nameObj = "Sudeepthi";
+            char[] values = { 'S', 'u', 'd', 'e', 'p', 'h','t','i' };
+            object myName = new string(values);
+            var doubleeql2 = (nameObj == myName);//false
+            var eql2 = nameObj.Equals(myName);//true
+            Console.WriteLine(doubleeql2);
+            Console.WriteLine(eql2);
+        }
+    }
+}
