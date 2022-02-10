836aa32260adaf666853a0eb69bf3663e22ab8cf
sudeepthi <telugusudeepthi001@gmail.com>
2022-02-10 13:40:07 +0530

    day11

---
+++ day 11/day 11/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace day_11
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            Stack stack = new Stack();
+            int total = 3;
+
+            for (int i = 0; i < total; i++)
+            {
+                string name = Console.ReadLine();
+                int age = int.Parse(Console.ReadLine());
+
+                stack.Push(new Person()
+                {
+                    Name = name,
+                    Age = age
+                });
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Person p = (Person)stack.Pop();
+                Console.WriteLine(p.ToString());
+            }
+            
+        }
+        public class Person
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+
+
+            public override string ToString()
+            {
+                return Name + " - " + Age;
+            }
+        }
+    }
+}
