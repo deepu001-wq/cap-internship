a103f1777d36379bb734474a77157fdfcae82e63
sudeepthi <telugusudeepthi001@gmail.com>
2022-02-16 16:38:26 +0530

    studen table

---
+++ SQL/My Code Snippets/SQLQuery2.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Student 
+(Rollno varchar(30),Name varchar(30),gender varchar(200),class varchar(100))
+insert into Student values(101,'Vikash4','Male','cse-3')
+insert into Student values(111,'supriya','Female','cse-3')
+insert into Student values(121,'Vikas','Male','cse-3')
+insert into Student values(134,'Vishika','Female','ece-3')
+select * from Student
+create table course(courseid int,coursename varchar(100))
+select * from course
+insert into course values('4031','datastructures')
+insert into course values('4022','dotnet')
+insert into course values('4562','java')
+insert into course values('4786','C lang')
